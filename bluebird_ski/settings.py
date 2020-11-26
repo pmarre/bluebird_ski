7ec470ca45dc70045c16717e0a70882e303cfbad
Patrick Marre <pmarre40@gmail.com>
2020-11-25 18:10:32 -0800

    Changed and hid secret key

--- bluebird_ski/settings.py
+++ bluebird_ski/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from util import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3597w!13ormvd5whxhqq1sm&g+1#ihiescr8tsd*j-y#uf0)cb'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
