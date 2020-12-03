7aae9efaf3705a9381df95b98db96ba64e8c4ad1
Patrick Marre <pmarre40@gmail.com>
2020-12-03 14:37:41 -0800

    added service detail page and functionality

--- services/views.py
+++ services/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Service
 # Create your views here.
+
+
+def all_services(request):
+    ''' A view to show all services '''
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services
+    }
+
+    return render(request, 'services/services.html', context)
+
+
+def service_detail(request, service_id):
+    ''' A detail view of service '''
+
+    service = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service
+    }
+
+    return render(request, 'services/service_detail.html', context)
