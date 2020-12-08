73fe113778c010ffaba664e3eb8b00c80cf6311a
Patrick Marre <pmarre40@gmail.com>
2020-12-07 21:38:28 -0800

    added in shopping cart functionality

--- bluebird_ski/settings.py
+++ bluebird_ski/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'services',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
         },
     },
---
+++ cart/contexts.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    service_count = 0
+    cart = request.session.get('cart', {})
+    u_name = str(request.user)
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        total += quantity * service.price
+        service_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
+
+    if u_name == 'AnonymousUser':
+        print('not a member')
+    else:
+        discount = total * Decimal(0.1)
+        total = total - discount
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'service_count': service_count,
+    }
+
+    return context
