a49f1dd493b8216b382cf9950e84d8d0c2a6b851
Patrick Marre <pmarre40@gmail.com>
2020-12-10 15:50:26 -0800

    added stripe, working on debugging a few issues

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,137 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from services.models import Service
+from cart.contexts import cart_contents
 
-# Create your views here.
+import stripe
+import json
+
+
+# @require_POST
+# def cache_checkout_data(request):
+#     try:
+#         pid = request.POST.get('client_secret').split('_secret')[0]
+#         stripe.api_key = settings.STRIPE_SECRET_KEY
+#         stripe.PaymentIntent.modify(pid, metadata={
+#             'cart': json.dumps(request.session.get('cart', {})),
+#             'save_info': request.POST.get('save_info'),
+#             'username': request.user,
+#         })
+#         return HttpResponse(status=200)
+#     except Exception as e:
+#         messages.error(request, 'Sorry, your payment cannot be \
+#             processed right now. Please try again later.')
+#         return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your cart.")
-        return redirect(reverse('services'))
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            # pid = request.POST.get('client_secret').split('_secret')[0]
+            # order.stripe_pid = pid
+            # order.original_cart = json.dumps(cart)
+            order.save()
+            for item_id, item_data in cart.items():
+                try:
+                    service = Service.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            service=service,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            size = size.split(',')
+                            combined_size = '{},{}'.format(size[0], size[1])
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                service=service,
+                                quantity=quantity,
+                                boot_size=size[0],
+                                ski_size=size[1],
+                            )
+                            order_line_item.save()
+                except Service.DoesNotExist:
+                    messages.error(request, (
+                        "One of the services in your cart wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
+
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+    else:
+
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "There's nothing in your cart.")
+            return redirect(reverse('services'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
+
+        if not stripe_public_key:
+            messages.warning(
+                request, 'Stripe Public Key missing! Set with command export STRIPE_PUBLIC_KEY=<pk_key>')
+
+        order_form = OrderForm()
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+            'client_secret': intent.client_secret,
+        }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(
+        request, f'Order successful! Your order is {order_number}. A confirmation email will be sent to {order.email}.')
+    if 'cart' in request.session:
+        del request.session['cart']
+    template = 'checkout/checkout_success.html'
     context = {
-        'order_form': order_form,
+        'order': order,
     }
 
     return render(request, template, context)
