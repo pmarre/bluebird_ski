34f7919f6e4b48f8cadd6ab0a0d0cbfc7ebd10f1
Patrick Marre <pmarre40@gmail.com>
2020-12-13 15:55:02 -0800

    updated add/edit service form for better UI

--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .models import Service, Category
 from .forms import ServiceForm
 # Create your views here.
@@ -47,8 +49,14 @@ def service_detail(request, service_id):
     return render(request, 'services/service_detail.html', context)
 
 
+@login_required
 def add_service(request):
     """ Add a service to the store """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ServiceForm(request.POST, request.FILES)
         if form.is_valid():
@@ -69,8 +77,13 @@ def add_service(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_service(request, service_id):
     """ Edit a service in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     service = get_object_or_404(Service, pk=service_id)
     if request.method == 'POST':
         form = ServiceForm(request.POST, request.FILES, instance=service)
@@ -94,8 +107,13 @@ def edit_service(request, service_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_service(request, service_id):
     """ Delete a service from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     service = get_object_or_404(Service, pk=service_id)
     service.delete()
     messages.success(request, 'Service deleted!')
