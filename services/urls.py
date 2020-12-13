dabe916d72706f970393800cabcbecefc11f3416
Patrick Marre <pmarre40@gmail.com>
2020-12-13 15:18:24 -0800

    added service management functionality add/edit/delete

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
-    path('<service_id>', views.service_detail, name='service_detail')
+    path('<int:service_id>/', views.service_detail, name='service_detail'),
+    path('add/', views.add_service, name='add_service'),
+    path('edit/<int:service_id>/', views.edit_service, name='edit_service'),
+    path('delete/<int:service_id>/', views.delete_service, name='delete_service'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Service, Category
+from .forms import ServiceForm
 # Create your views here.
 
 
@@ -43,3 +45,58 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_detail.html', context)
+
+
+def add_service(request):
+    """ Add a service to the store """
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            service = form.save()
+            messages.success(request, 'Successfully added service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(
+                request, 'Failed to add service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm()
+
+    template = 'services/add_service.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_service(request, service_id):
+    """ Edit a service in the store """
+    service = get_object_or_404(Service, pk=service_id)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(
+                request, 'Failed to update service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm(instance=service)
+        messages.info(request, f'You are editing {service.name}')
+
+    template = 'services/edit_service.html'
+    context = {
+        'form': form,
+        'service': service,
+    }
+
+    return render(request, template, context)
+
+
+def delete_service(request, service_id):
+    """ Delete a service from the store """
+    service = get_object_or_404(Service, pk=service_id)
+    service.delete()
+    messages.success(request, 'Service deleted!')
+    return redirect(reverse('services'))
