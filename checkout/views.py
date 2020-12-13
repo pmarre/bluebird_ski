50684f795d659bac4cda0bbb541f7caefdfe359b
Patrick Marre <pmarre40@gmail.com>
2020-12-13 14:10:02 -0800

    completed profile functionality

--- checkout/views.py
+++ checkout/views.py
@@ -5,7 +5,10 @@ from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from services.models import Service
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from cart.contexts import cart_contents
 
 import stripe
@@ -47,6 +50,7 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
@@ -67,7 +71,6 @@ def checkout(request):
                     else:
                         for size, quantity in item_data['items_by_size'].items():
                             size = size.split(',')
-                            combined_size = '{},{}'.format(size[0], size[1])
                             order_line_item = OrderLineItem(
                                 order=order,
                                 service=service,
@@ -76,7 +79,7 @@ def checkout(request):
                                 ski_size=size[1],
                             )
                             order_line_item.save()
-                except Service.DoesNotExist:
+                except service.DoesNotExist:
                     messages.error(request, (
                         "One of the services in your cart wasn't found in our database. "
                         "Please call us for assistance!")
@@ -84,17 +87,17 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_cart'))
 
+            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
-
     else:
-
         cart = request.session.get('cart', {})
         if not cart:
-            messages.error(request, "There's nothing in your cart.")
+            messages.error(
+                request, "There's nothing in your cart at the moment")
             return redirect(reverse('services'))
 
         current_cart = cart_contents(request)
@@ -103,32 +106,78 @@ def checkout(request):
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY
+            currency=settings.STRIPE_CURRENCY,
         )
 
-        if not stripe_public_key:
-            messages.warning(
-                request, 'Stripe Public Key missing! Set with command export STRIPE_PUBLIC_KEY=<pk_key>')
+        # Attempt to prefill the form with any info the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
-        order_form = OrderForm()
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
-            'client_secret': intent.client_secret,
-        }
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
 
     return render(request, template, context)
 
 
 def checkout_success(request, order_number):
-
+    """
+    Handle successful checkouts
+    """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(
-        request, f'Order successful! Your order is {order_number}. A confirmation email will be sent to {order.email}.')
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
     if 'cart' in request.session:
         del request.session['cart']
+
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
