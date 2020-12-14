20a4bc0dea67f60ff349c53c41224e85a95474fd
Patrick Marre <pmarre40@gmail.com>
2020-12-14 11:03:39 -0800

    heroku deployment attempt 2

--- bluebird_ski/settings.py
+++ bluebird_ski/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+# check if using heroku or local dev environ
 if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 else:
