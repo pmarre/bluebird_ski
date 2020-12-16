c44fc09a0ba84dd8918085e7b89ad67fdd03a021
Patrick Marre <pmarre40@gmail.com>
2020-12-16 14:53:15 -0800

    failed launch, debugging

--- bluebird_ski/settings.py
+++ bluebird_ski/settings.py
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = ['bluebird-ski-shop.herokuapp.com', 'localhost', '127.0.0.1']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes', pyth
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
