cddd080e7cb9259f20c3c8f1f383521e71f390eb
Patrick Marre <pmarre40@gmail.com>
2020-12-19 09:49:34 -0800

    fixed email location error

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,7 +22,7 @@ class StripeWH_Handler:
         """Send the user a confirmation email"""
         if order.shop_location == 'sf':
             shop_location = '1616 Mountain View Avenue\nSan Francisco, CA 94016\n415-555-2916\nOpen 7:00 AM - 5:00 PM, daily'
-        elif order.shop_location == 'sf':
+        elif order.shop_location == 'tahoe':
             shop_location = '13589 Main Street\nTahoe City, CA 96145\n530-555-1616\nOpen 7:00 AM - 5:00 PM, daily'
         else:
             shop_location = '1248 Lake View Street\nBishop, CA 93514\n760-555-1304\nOpen 7:00 AM - 5:00 PM, daily'
